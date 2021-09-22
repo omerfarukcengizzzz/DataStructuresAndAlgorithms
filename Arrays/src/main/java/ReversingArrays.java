038acc261ef81227010f16630f2746858a76a715
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-09-22 18:34:36 +0100

    Reversing Array

--- Arrays/src/main/java/ReversingArrays.java
+++ Arrays/src/main/java/ReversingArrays.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class ReversingArrays {
     public static void main(String[] args) {
 
-        int[] arr = {1, 3, 2, 5, 4};
+        int[] arr = {1, 3, 2, 5, 4, -1};
 
         System.out.println(Arrays.toString(reverseArray(arr)));
 
@@ -12,31 +12,17 @@ public class ReversingArrays {
 
     public static int[] reverseArray(int[] arr) {
 
-        if (arr.length % 2 == 1) {
+        for (int i = 0; i <= (arr.length / 2) - 1; i++) {
 
-            for (int i = 0; i <= (arr.length / 2) - 1; i++) {
+            int temp = 0;
 
-                if (i == 0) {
-                        int temp = 0;
-
-                        temp = arr[i] + arr[arr.length-1];
-                        arr[arr.length-1] = temp - arr[arr.length-1];
-                        arr[i] = temp - arr[arr.length-1];
-
-                } else if (i == 1) {
-
-                        int temp = 0;
-
-                        temp = arr[i] + arr[arr.length-2];
-                        arr[arr.length-2] = temp - arr[arr.length-2];
-                        arr[i] = temp - arr[arr.length-2];
-
-                }
-
-            }
+            temp = arr[i] + arr[(arr.length - 1) - i];
+            arr[(arr.length - 1) - i] = temp - arr[(arr.length - 1) - i];
+            arr[i] = temp - arr[(arr.length - 1) - i];
 
         }
 
         return arr;
     }
+
 }
