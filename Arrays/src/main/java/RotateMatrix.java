ede9df4672f39c3ec77bfa2dafe3cb1458ec161f
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-06 20:49:00 +0100

    Rotate Matrix

--- Arrays/src/main/java/RotateMatrix.java
+++ Arrays/src/main/java/RotateMatrix.java
@@ -22,8 +22,10 @@ public class RotateMatrix {
             int n = matrix.length;
 
             for (int layer = 0; layer < n / 2; layer++) {
+
                 int first = layer;
                 int last = n - 1 - layer;
+
                 System.out.println("Initial Matrix at Layer : " + layer);
                 printMatrix(matrix);
 
