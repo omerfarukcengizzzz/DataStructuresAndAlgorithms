2a4b3cd46a808fc3d85c1228c289d8bed653edc4
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-07 23:37:09 +0100

    LinkedList

--- LinkedList/src/main/java/CyberLinkedList.java
+++ LinkedList/src/main/java/CyberLinkedList.java
@@ -1,2 +1,16 @@
 public class CyberLinkedList {
+
+    public class Node {
+
+        public int value;
+        public Node next;   // pointer (address) for the next Node object
+
+        // constructor with value parameter
+        public Node(int value) {
+            this.value = value;
+        }
+
+    }
+
+
 }
