2d055e6b24f8bb683a003e7e16ec728e0441f9b6
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2021-10-07 23:40:11 +0100

    LinkedList

--- LinkedList/src/main/java/CyberLinkedList.java
+++ LinkedList/src/main/java/CyberLinkedList.java
@@ -10,5 +10,15 @@ public class CyberLinkedList {
         }
     }
 
+    public Node first;
+    public Node last;
+    public int size;
 
+    // when ever the CyberLinkedList object is created
+    // the initial values for the parameters will be set
+    public CyberLinkedList() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 }
